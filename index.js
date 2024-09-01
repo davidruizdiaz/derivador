59e971a463356400c9fdfeab5ce8285840641122
davidruizdiaz <adavidruizdiaz@gmail.com>
2024-09-01 08:53:23 -0400

    Modificación en el readme

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ programInit();
 
 
 // TODO: Tareas
-// 1. crear carpeta dist                                      ✔️
-// 2. crear arbol de carpetas para la opción seleccionada
-// 3. Cargar los assets/commons en la carpetas respectivas
-// 4. Crear los archivos variables con los assets/variables
-// 5. Crear dataBase
-// 7. Realizar pruebas                                        
+// 1. crear carpeta dist                                          ✔️
+// 2. crear arbol de carpetas para la opción seleccionada         ✔️
+// 3. Cargar los coreAssets/shared en la carpetas respectivas     ✔️
+// 4. Cargar los coreAssets/notShared en la carpetas respectivas  ✔️
+// 5. Crear los archivos variables con los coreAssets/variables   
+// 6. Crear dataBase
+// 7. Si da crear documentación
+// 8. Realizar pruebas
